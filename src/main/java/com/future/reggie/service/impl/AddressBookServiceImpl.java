c83ae5b4d51386c0e4adffb988bce5c4db43c2f7
MrNeoJeep <mrneo@foxmail.com>
2022-09-22 16:51:34 +0800

    创建新分支，进行优化

--- src/main/java/com/future/reggie/service/impl/AddressBookServiceImpl.java
+++ src/main/java/com/future/reggie/service/impl/AddressBookServiceImpl.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.future.reggie.mapper.AddressBookMapper;
 import com.future.reggie.pojo.AddressBook;
 import com.future.reggie.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class AddressBookServiceImpl extends ServiceImpl<AddressBookMapper, AddressBook> implements AddressBookService {
 
